658a5b9b4faffbd58703175d9b0dab6489275090
fauziahmad37 <fauzi.enginer@gmail.com>
2023-04-12 10:05:05 +0700

    comment social media login and change input username placeholder

--- application/views/header.php
+++ application/views/header.php
@@ -39,7 +39,7 @@
         	<div class="row">
         		 <label for="username">
                     Username:
-                    <input type="text" name="username" id="username" placeholder="Hugh Jackman" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{8,20}$" required="required" />
+                    <input type="text" name="username" id="username" placeholder="" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{8,20}$" required="required" />
                 </label>
         	</div>
            
@@ -61,13 +61,13 @@
            	 <button type="submit">Login</button>
            </div>
         </form>
-        <div class="row">
+        <!-- <div class="row">
         	<p>Or via social</p>
             <div class="social-btn-2">
             	<a class="fb" href="#"><i class="ion-social-facebook"></i>Facebook</a>
             	<a class="tw" href="#"><i class="ion-social-twitter"></i>twitter</a>
             </div>
-        </div>
+        </div> -->
     </div>
 </div>
 <!--end of login form popup-->
