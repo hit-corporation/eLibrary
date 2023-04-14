c63e213d9d80691f97df58ef560f48326dbc53ff
fauziahmad37 <fauzi.enginer@gmail.com>
2023-04-14 11:12:56 +0700

    add parameter dan argument view_group  pada get_all di controller Book

--- application/controllers/Book.php
+++ application/controllers/Book.php
@@ -11,17 +11,7 @@ class Book extends MY_Controller {
 	}
 
 	public function index(){
-		// $page = $this->input->post('page');
-		// $page = $page ? $page : 1;
-		// $limit = 5;
-		// $limit_start = ($page - 1) * $limit;
-		// // $no = $limit_start + 1;
-
-		// $data['books'] 	= $this->home_model->get_books($limit, $limit_start);
-
-		// $data['total_records'] = $this->home_model->get_total_books();
-
-		// $data['newBooks'] 	= $this->home_model->get_new_books();
+		$data['viewGroup'] = $_GET['viewGroup'];
 
 		$data['publishers'] = $this->publisher_model->get_all();
 		$data['categories'] = $this->kategori_model->get_all();
@@ -32,8 +22,10 @@ class Book extends MY_Controller {
 	}
 
 	public function get_all(){
+		$view_group 	= $_GET['view_group'];
 		$page 			= isset($_GET['page']) ? $_GET['page'] : 1;
 		$limit 			= isset($_GET['limit']) ? $_GET['limit'] : 3;
+
 		$title 			= isset($_GET['title']) ? $_GET['title'] : '';
 		$publisher_id 	= isset($_GET['publisher_id']) ? $_GET['publisher_id'] : '';
 		$author 		= isset($_GET['author']) ? $_GET['author'] : '';
@@ -42,7 +34,7 @@ class Book extends MY_Controller {
 
 		$page = ($page - 1) * $limit;
 
-		$data['books'] 	= $this->home_model->get_books($limit, $page, $title, $publisher_id, $author, $categori_ids, $year);
+		$data['books'] 	= $this->home_model->get_books($view_group, $limit, $page, $title, $publisher_id, $author, $categori_ids, $year);
 		$data['total_records'] = $this->home_model->get_total_books();
 		$data['total_pages'] = ceil($data['total_records'] / $limit);
 		
