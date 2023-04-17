702351acc9dc6187b34250bab4268ee63549072c
fauziahmad37 <fauzi.enginer@gmail.com>
2023-04-17 09:23:54 +0700

    create user data and check user login pada controller User

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,11 +6,24 @@ class User extends MY_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->load->model(['member_model']);
+
+		// load session library
+		$this->load->library('session');
+
+		// check if user is logged in
+		if (!$this->session->userdata('user')['is_logged_in']) {
+			redirect('login');
+		}
 	}
 
 	public function index(){
+		// get user data
+		$userId 		= $this->member_model->get_user_by_username($this->session->userdata('user')['user_name'])['id'];
+		$data['user'] 	= $this->member_model->get_user($userId);
+
+
 		$this->load->view('header');
-		$this->load->view('home/user_profile');
+		$this->load->view('home/user_profile', $data);
 		$this->load->view('footer');
 	}
 
