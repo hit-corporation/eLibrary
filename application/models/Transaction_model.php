7741ab0abbf4f02e57119555e70933c3b92959f3
fauziahmad37 <fauzi.enginer@gmail.com>
2023-04-28 11:26:45 +0700

    create query check_borrowed pada model transaction_model

--- application/models/Transaction_model.php
+++ application/models/Transaction_model.php
@@ -25,4 +25,45 @@ class Transaction_model extends CI_Model {
 	public function avg_read_time() {
 		$query = "SELECT ";		
 	}
+
+	// check_borrowed
+
+	/**
+	 * Query for check if book is borrowed
+	 * 
+	 * @param int $book_id
+	 * @param int $user_id
+	 * 
+	 * @return array
+	 */
+	 
+	public function check_borrowed(int $book_id, int $user_id): array {
+		$this->db->from('transactions');
+		$this->db->where('book_id', $book_id);
+		$this->db->where('member_id', $user_id);
+		$this->db->where('actual_return', null);
+		$this->db->order_by('id', 'DESC');
+		$this->db->limit(1);
+
+		$res = $this->db->get();
+		return $res->row_array();
+	}
+
+	/**
+	 * Query for get user borrowed book
+	 * 
+	 * @param int $user_id
+	 *
+	 * @return int
+	 */
+	 
+	public function get_user_borrowed_book(int $user_id): int {
+		$this->db->from('transactions');
+		$this->db->where('member_id', $user_id);
+		$this->db->where('actual_return', null);
+
+		$res = $this->db->get();
+		return $res->num_rows();
+	}
+
 }
