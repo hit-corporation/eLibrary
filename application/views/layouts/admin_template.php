97dcd60c80304321dc81af1c74030a697510a5f0
fauziahmad37 <fauzi.enginer@gmail.com>
2023-05-05 10:10:52 +0700

    delete menu order/return_order

--- application/views/layouts/admin_template.php
+++ application/views/layouts/admin_template.php
@@ -153,13 +153,6 @@
                 </div>
             </li>
 
-			<!-- Nav Item - Pengembalian -->
-			<li class="nav-item <?=($CI->uri->segment(1) == 'admin' && $CI->uri->segment(2) == 'order' && $CI->uri->segment(3) == 'return_order') ? 'active bg-dark-2' : '' ?>">
-                <a class="nav-link" href="<?=base_url('admin/order/return_order')?>">
-                    <i class="fa fa-reply-all" aria-hidden="true"></i>
-                    <span>Pengembalian</span></a>
-            </li>
-
             <!-- Nav Item - Book -->
             <li class="nav-item <?=$CI->uri->segment(1) == 'admin' && $CI->uri->segment(2) == 'book' ? 'active bg-dark-2' : '' ?>">
                 <a class="nav-link" href="<?=base_url('admin/book')?>">
